e049b2e9150b384544860cd56a76ec8f414915f1
Nhi Nguyen <nhi.nguyen@yale.edu>
2021-01-18 13:35:07 +0700

    Update function description

--- 210115/index.js
+++ 210115/index.js
@@ -1,8 +1,10 @@
 // Sử dụng các hàm đọc, ghi file đồng bộ để hoàn thiện các yêu cầu sau
+// Use readFileSync and writeFileSync to implement the following functions
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./data.json').toString());
 
-// lấy thông tin học sinh có _id là jubuq3lfmjjmp0wrdeupt
+// Lấy thông tin học sinh có _id là jubuq3lfmjjmp0wrdeupt
+// Get the information of the student whose _id is jubuq3lfmjjmp0wrdeupt
 function getDetailStudent() {
   const _id = 'jubuq3lfmjjmp0wrdeupt';
   for(let student of data){
@@ -12,16 +14,18 @@ function getDetailStudent() {
 }
 
 // Lấy số lượng học sinh có từ Nguyễn
+// Get the number of students whose name includes Nguyễn
 function getCountStudentWithLastName() {
   const lastName = 'Nguyễn';
   let count = 0;
   for(let student of data){
-    if(student.name.split(' ')[0] === lastName) count ++;
+    if(student.name.includes(lastName)) count ++;
   }
   return count;
 }
 
 // Tính điểm trung bình của toàn bộ sinh viên (làm tròn đến một chữ số sau dấu phẩy)
+// Calculate the average mark of all students (one digit after the decimal point)
 function calAverageMark() {
   let total = count = 0;
   for(let student of data){
@@ -31,7 +35,8 @@ function calAverageMark() {
   return parseFloat((total / count).toFixed(1));
 }
 
-// Ghi ra số lượng học sinh đạt điểm 10 ra file output.txt (sử dụng hàm ghi đồng bộ);
+// Ghi ra số lượng học sinh đạt điểm 10 ra file output.txt (sử dụng hàm ghi đồng bộ)
+// Write to output.txt the number of students with mark 10 (using writeFileSync)
 function writeCountStudentGet10MarkToFile() {
   let count = 0
   for(let student of data){
